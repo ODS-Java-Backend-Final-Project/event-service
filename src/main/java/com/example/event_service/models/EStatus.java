a01af77682ea48505d88200447d8458a8d58adde
anruiz-r <angela.ruiz.rodriguez.8@gmail.com>
2025-05-24 21:01:48 +0200

    modelo evento creado

---
+++ src/main/java/com/example/event_service/models/EStatus.java
@@ -0,0 +1,8 @@
+package com.example.event_service.models;
+
+public enum EStatus {
+    UPCOMING,
+    ON_GOING,
+    COMPLETED,
+    CANCELLED
+}
---
+++ src/main/java/com/example/event_service/models/Event.java
@@ -0,0 +1,29 @@
+package com.example.event_service.models;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Event {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String description;
+    private String date;
+    private String location;
+    private String organizer;
+    private EStatus status;
+    private List<String> atendees;
+    private Long boardGameId;
+}

ce38e68c8d1bbdcd30a5c69df0be615db3081bb0
anruiz-r <angela.ruiz.rodriguez.8@gmail.com>
2025-05-26 10:31:45 +0200

    repositorio

---
+++ src/main/java/com/example/event_service/repositories/EventRepository.java
@@ -0,0 +1,9 @@
+package com.example.event_service.repositories;
+
+import com.example.event_service.models.Event;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface EventRepository extends JpaRepository<Event, Long> {
+}

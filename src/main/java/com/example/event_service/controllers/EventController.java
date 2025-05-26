f9d9bbbfcdf0490cb79946fe4ee059ca4eb68135
anruiz-r <angela.ruiz.rodriguez.8@gmail.com>
2025-05-26 16:35:03 +0200

    ruta getAll y create

--- src/main/java/com/example/event_service/controllers/EventController.java
+++ src/main/java/com/example/event_service/controllers/EventController.java
@@ -3,10 +3,12 @@ package com.example.event_service.controllers;
 import com.example.event_service.exceptions.EventNotFoundException;
 import com.example.event_service.models.Event;
 import com.example.event_service.services.EventService;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 
 @RestController
 @RequestMapping("/api/events")
@@ -23,4 +25,36 @@ public class EventController {
             return new ResponseEntity<>( e.getMessage(), HttpStatus.NOT_FOUND);
         }
     }
+
+    @GetMapping
+    @ResponseStatus(HttpStatus.OK)
+    public ResponseEntity<?> findAllEvents() {
+        return new ResponseEntity<>(eventService.findAll(), HttpStatus.OK);
+    }
+
+    @DeleteMapping("/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void deleteEventById(@PathVariable Long id) {
+        try {
+            eventService.deleteEventById(id);
+        } catch (EventNotFoundException e) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, e.getMessage());
+        }
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public Event createEvent(@RequestBody Event event) {
+        return eventService.saveEvent(event);
+    }
+//
+//    @PutMapping("/{id}")
+//    @ResponseStatus(HttpStatus.OK)
+//    public Event updateEvent(@PathVariable Long id, @RequestBody @Valid Event event) {
+//        try {
+//            return eventService.updateEvent(id, event);
+//        } catch (EventNotFoundException e) {
+//            throw new ResponseStatusException(HttpStatus.NOT_FOUND, e.getMessage());
+//        }
+  //  }
 }
--- src/main/java/com/example/event_service/services/EventService.java
+++ src/main/java/com/example/event_service/services/EventService.java
@@ -4,8 +4,11 @@ import com.example.event_service.exceptions.EventNotFoundException;
 import com.example.event_service.models.Event;
 import com.example.event_service.repositories.EventRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
 
+import java.util.List;
 import java.util.Optional;
 
 @Service
@@ -21,4 +24,38 @@ public class EventService {
             throw new EventNotFoundException("Event with ID " + id + " not found.");
         }
     }
+
+    public Event saveEvent(Event event) {
+        return eventRepository.save(event);
+    }
+
+    public void deleteEventById(Long id) throws EventNotFoundException {
+        Optional<Event> foundEvent = eventRepository.findById(id);
+        if (foundEvent.isPresent()) {
+            eventRepository.deleteById(id);
+        } else {
+            throw new EventNotFoundException("Event with ID " + id + " not found.");
+        }
+    }
+
+//    public Event updateEvent(Long id, Event event) throws EventNotFoundException {
+//        Event existingEvent = eventRepository.findById(id).orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Event with ID " + id + " not found."));
+//        existingEvent.setName(event.getName());
+//        existingEvent.setDescription(event.getDescription());
+//        existingEvent.setDate(event.getDate());
+//        existingEvent.setLocation(event.getLocation());
+//        existingEvent.setOrganizer(event.getOrganizer());
+//        existingEvent.setAttendees(event.getAttendees());
+//        existingEvent.setStatus(event.getStatus());
+
+
+//        existingEvent.setBoardGameId(boardGameClient.getBoardGameId());
+//
+//        return eventRepository.save(existingEvent);
+//    }
+
+    public List<Event> findAll() {
+        return eventRepository.findAll();
+    }
+
 }

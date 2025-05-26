30a206199a5de569217e9be091a498876e4e0ecb
anruiz-r <angela.ruiz.rodriguez.8@gmail.com>
2025-05-26 10:56:10 +0200

    event controller ruta get by id

---
+++ src/main/java/com/example/event_service/controllers/EventController.java
@@ -0,0 +1,26 @@
+package com.example.event_service.controllers;
+
+import com.example.event_service.exceptions.EventNotFoundException;
+import com.example.event_service.models.Event;
+import com.example.event_service.services.EventService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/events")
+public class EventController {
+    @Autowired
+    EventService eventService;
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Event> getEventById(@PathVariable Long id) {
+        try {
+            Event foundEvent = eventService.findEventById(id);
+            return new ResponseEntity<>(foundEvent, HttpStatus.OK);
+        } catch (EventNotFoundException e) {
+            return new ResponseEntity<>( HttpStatus.NOT_FOUND);
+        }
+    }
+}
